cca95e83a237282b97c9eb128a0a507363050df4
Strawberries <48771826+Quill4184@users.noreply.github.com>
2019-03-20 13:38:01 -0700

    initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static int PlayerScore1 = 0;
+    public static int PlayerScore2 = 0;
+
+    public GUISkin layout;
+
+    GameObject theBall;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theBall = GameObject.FindGameObjectWithTag("Ball");
+    }
+
+    public static void Score(string wallID)
+    {
+        if (wallID == "Rightwall")
+
+            void Update()
+            {
+                PlayerScore1++;
+            }
+
+        else
+        {
+            PlayerScore2++;
+        }
+    }
+}
